a8ad79fef437c31b0e36bfeecd65ce4d66ba0a05
Behnam <behnam.sep@gmail.com>
2025-04-23 22:44:10 +0200

    feat: enhance tab navigation with icons and titles for better user experience

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,99 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+
+const TabsIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground className="flex  bg-purple-300 flex-row w-full flex-1 min-w-[114px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
+        <Image source={icon} className="size-5" tintColor="#151312" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
 
 const _layout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{title:'Home', headerShown: false }} />
-        <Tabs.Screen name="saved" options={{ title:'Saved',headerShown: false }} />
-        <Tabs.Screen name="serach" options={{title:'Search', headerShown: false }} />
-        <Tabs.Screen name="profile"  options={{ title:'Profile', headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignContent: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabsIcon title="Home" icon={icons.home} focused={true} />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabsIcon title="Saved" icon={icons.save} />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="serach"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabsIcon title="Search" icon={icons.search} />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabsIcon title="Profile" icon={icons.person} />
+            </>
+          ),
+        }}
+      />
     </Tabs>
   );
 };
