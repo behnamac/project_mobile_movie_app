f51d97a9df29f94baa93bf0820425f3b11871c85
Behnam Sepehri <behnam.sep@gmail.com>
2025-05-19 16:29:49 +0200

    feat: enhance MovieDetails component with detailed movie information and layout

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,16 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
+import { icons } from "@/constants/icons";
 
 const MovieDetails = () => {
-    const { id } = useLocalSearchParams()
+  const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
   return (
-    <View>
-      <Text>{id}</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `http://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px] bg-light-200 rounded-b-3xl"
+            resizeMode="cover"
+          />
+        </View>
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+          <View className="flex-row items-center gap-x-1 mt-2">
+            <Text className="text-light-200 text-sm">
+              {movie?.release_date?.split("_")[0]}
+            </Text>
+            <Text className="text-light-200 text-sm">|</Text>
+            <Text className="text-light-200 text-sm">{movie?.runtime}m </Text>
+          </View>
+          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-4" />
+            <Text className="text-light-200 text-sm font-bold">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className="text-light-200 text-sm font-bold">
+              ({movie?.vote_count} votes)
+            </Text>{" "}
+          </View>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
 export default MovieDetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
